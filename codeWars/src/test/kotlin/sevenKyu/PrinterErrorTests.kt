18bd1f18226bda21e71b1c35a927ae47544a00e7
Nathan Stasin <nathanstasin@gmail.com>
2020-12-18 13:11:01 +0200

    printerErrorTests

---
+++ codeWars/src/test/kotlin/sevenKyu/PrinterErrorTests.kt
@@ -0,0 +1,31 @@
+package sevenKyu
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class PrinterErrorTests {
+    /*
+    In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+    The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+    Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+    You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string.
+    Don't reduce this fraction to a simpler expression.
+     */
+    @Test
+    fun testFixed() {
+        assertThat(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")).isEqualTo("3/56")
+        assertThat(printerError("kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")).isEqualTo("6/60")
+        assertThat(printerError("kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyzuuuuu")).isEqualTo("11/65")
+        assertThat(printerError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmm")).isEqualTo("0/53")
+    }
+
+    fun printerError(s: String): String {
+        val (errors, noErrors) = s.fold(Pair(0, 0)) { acc: Pair<Int, Int>, c: Char ->
+            val newError = if (c in 'a'..'m') acc.first + 0 else acc.first + 1
+            val noError = if (c in 'a'..'m') acc.second + 1 else acc.second + 1
+            Pair(newError, noError)
+        }
+
+        return "$errors/$noErrors"
+    }
+}
